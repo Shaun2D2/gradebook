5017cf2e205b81bcf93b3ca1f058142e59988faa
Shaun Lobisnger <Shaun.lobsinger@gmail.com>
2019-07-14 20:47:29 -0700

    adding Math class fun!

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,23 +7,31 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            var book = new Book("Shaun's gradebook");
+
+            book.addGrade(89.1);
+            book.addGrade(15.1);
+            book.addGrade(67.1);
+
             var grades = new List<double>() { 100, 90.5, 60.6 };
             grades.Add(85.5);
 
+
+
             var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
             foreach(var number in grades) {
+                highGrade = Math.Max(number, highGrade);
+                lowGrade = Math.Min(number, lowGrade);
                 result += number;
             }
 
             result /= grades.Count;
 
             Console.WriteLine($"The average grade is {result:N1}");
-
-            if (args.Length > 0) {
-                Console.WriteLine($"Hello, {args[0]}!");
-            } else {
-                Console.WriteLine("Hello World!");
-            }
+            Console.WriteLine($"The lowest grade is {lowGrade:N1}");
+            Console.WriteLine($"The highest grade is {highGrade:N1}");
         }
     }
 }
