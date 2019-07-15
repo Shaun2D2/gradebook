573e5a54a1456c73dfe947105e560cf14ab2250c
Shaun Lobisnger <Shaun.lobsinger@gmail.com>
2019-07-14 18:59:31 -0700

    added some stuff together and wrote it out baby.

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,6 +6,11 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            double x = 34.1;
+            double y = 50.1;
+
+            Console.WriteLine($"The scores have been added to {x + y}");
+
             if (args.Length > 0) {
                 Console.WriteLine($"Hello, {args[0]}!");
             } else {
