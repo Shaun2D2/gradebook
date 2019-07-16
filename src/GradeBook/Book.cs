f990e3cd03da691a59e18bd544a25354950259a8
Shaun Lobisnger <Shaun.lobsinger@gmail.com>
2019-07-15 18:58:26 -0700

    added new ShowStatics method to Book class

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -10,9 +10,28 @@ namespace GradeBook {
             this.name = name;
         }
 
-        public void addGrade(double grade) 
+        public void AddGrade(double grade) 
         {
             grades.Add(grade);
+        }
+
+        public void ShowStatics()
+        {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            
+            foreach(var number in grades) {
+                highGrade = Math.Max(number, highGrade);
+                lowGrade = Math.Min(number, lowGrade);
+                result += number;
+            }
+
+            result /= grades.Count;
+
+            Console.WriteLine($"The average grade is {result:N1}");
+            Console.WriteLine($"The lowest grade is {lowGrade:N1}");
+            Console.WriteLine($"The highest grade is {highGrade:N1}");
         }
 
         private List<double> grades;
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -9,29 +9,10 @@ namespace GradeBook
         {
             var book = new Book("Shaun's gradebook");
 
-            book.addGrade(89.1);
-            book.addGrade(15.1);
-            book.addGrade(67.1);
-
-            var grades = new List<double>() { 100, 90.5, 60.6 };
-            grades.Add(85.5);
-
-
-
-            var result = 0.0;
-            var highGrade = double.MinValue;
-            var lowGrade = double.MaxValue;
-            foreach(var number in grades) {
-                highGrade = Math.Max(number, highGrade);
-                lowGrade = Math.Min(number, lowGrade);
-                result += number;
-            }
-
-            result /= grades.Count;
-
-            Console.WriteLine($"The average grade is {result:N1}");
-            Console.WriteLine($"The lowest grade is {lowGrade:N1}");
-            Console.WriteLine($"The highest grade is {highGrade:N1}");
+            book.AddGrade(89.1);
+            book.AddGrade(15.1);
+            book.AddGrade(67.1);
+            book.ShowStatics();
         }
     }
 }
