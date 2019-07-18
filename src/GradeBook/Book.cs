578e22de0c0f7e276a258b757f804450ebea624f
Shaun Lobisnger <Shaun.lobsinger@gmail.com>
2019-07-17 19:37:24 -0700

    delegates!

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -10,9 +10,32 @@ namespace GradeBook {
             Name = name;
         }
 
+        public void AddLetterGrade(char letter)
+        {
+            switch(letter) {
+                case 'A':
+                    AddGrade(90);
+                    break;
+                case 'B':
+                    AddGrade(80);
+                    break;
+                case 'C':
+                    AddGrade(70);
+                    break;
+                default:
+                    AddGrade(0);                
+                    break;
+            }
+        }
+
         public void AddGrade(double grade) 
         {
-            grades.Add(grade);
+            if (grade <= 100 && grade >= 0) {
+                grades.Add(grade);
+            } else {
+                throw new ArgumentException($"Invalid Grade {nameof(grade)}");
+            }
+
         }
 
         public Statistics GetStatics()
@@ -22,18 +45,60 @@ namespace GradeBook {
             result.High = double.MinValue;
             result.Low = double.MaxValue;
             
-            foreach(var grade in grades) {
-                result.High = Math.Max(grade, result.High);
-                result.Low = Math.Min(grade, result.Low);
-                result.Average += grade;
-            }
+            var index = 0;
+            do {
+                result.High = Math.Max(grades[index], result.High);
+                result.Low = Math.Min(grades[index], result.Low);
+                result.Average += grades[index];
+
+                index += 1;
+            } while (index < grades.Count);
 
             result.Average /= grades.Count;
+
+            switch(result.Average) {
+                case var d when d >= 90.0:
+                    result.Letter = 'A';
+                    break;
+                case var d when d >= 80.0:
+                    result.Letter = 'B';
+                    break;
+                case var d when d >= 70.0:
+                    result.Letter = 'C';
+                    break;
+                case var d when d >= 60.0:
+                    result.Letter = 'D';
+                    break;
+                default:
+                    result.Letter = 'F';
+                    break;                
+            }
 
             return result;
         }
 
+        // private string category;
         private List<double> grades;
-        public String Name;
+        public String Name
+        {
+            get;
+            set;
+        }
+
+        // public String Name
+        // {
+        //     get
+        //     {
+        //         return name;
+        //     }
+        //     set
+        //     {
+        //         name = value;
+        //     }
+        // }
+        public String name;
+
+        // readonly string category = "Science";
+        // public const string CATEGORY = "Science";
     }
 }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -9,14 +9,29 @@ namespace GradeBook
         {
             var book = new Book("Shaun's gradebook");
 
-            book.AddGrade(89.1);
-            book.AddGrade(15.1);
-            book.AddGrade(67.1);
+            while (true) {
+                Console.WriteLine("Enter a grade or exit with 'q'");
+
+                var input = Console.ReadLine();
+
+                if (input == "q") {
+                    break;
+                }
+
+                try {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                } catch(Exception ex) {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
             var stats = book.GetStatics();
 
             Console.WriteLine($"The average grade is {stats.Average:N1}");
             Console.WriteLine($"The lowest grade is {stats.Low:N1}");
             Console.WriteLine($"The highest grade is {stats.High:N1}");
+            Console.WriteLine($"The letter is {stats.Letter}");
         }
     }
 }
